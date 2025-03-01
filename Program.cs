9152192a03984dea49dc0b201666c864ddf54a7c
Franck <franckdipanda@hotmail.com>
2025-02-28 22:04:29 -0500

    Commit 1.0

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Task.Run(() => StartServer()); // Start the server in a separate task
+        Thread.Sleep(500); // Ensure server starts before client
+        StartClient(); // Start the client
+    }
+
+    static void StartServer()
+    {
+        using (NamedPipeServerStream server = new NamedPipeServerStream("mypipe"))
+        {
+            Console.WriteLine("[Server] Waiting for connection...");
+            server.WaitForConnection();
+            Console.WriteLine("[Server] Client connected.");
+
+            using (StreamReader reader = new StreamReader(server))
+            {
+                string message = reader.ReadLine();
+                Console.WriteLine("[Server] Received: " + message);
+            }
+        }
+    }
+
+    static void StartClient()
+    {
+        using (NamedPipeClientStream client = new NamedPipeClientStream("mypipe"))
+        {
+            client.Connect();
+            Console.WriteLine("[Client] Connected to server.");
+
+            using (StreamWriter writer = new StreamWriter(client))
+            {
+                writer.AutoFlush = true;
+                writer.WriteLine("Hello from Client!");
+                Console.WriteLine("[Client] Message sent.");
+            }
+        }
+    }
+}
